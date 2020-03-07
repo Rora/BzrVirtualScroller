3bb728ece8fd97e81d1ea429342ab227aa5402cf
Rob <2895595+Rora@users.noreply.github.com>
2020-03-07 22:00:22 +0100

    added JS interop to scroll to the correct location when prepending items

---
+++ BzrVirtualScroller/wwwroot/BzrVirtualScroller.js
@@ -0,0 +1,25 @@
+﻿window.bzrVirtualScroller = {
+    
+    scrollPastTopItems: (containerElement, nrOfItems) => {
+
+        var items = containerElement.children;
+        var totalHeightToScroll = 0;
+
+        for (var i = 0; i < nrOfItems ; i++) {
+            totalHeightToScroll += bzrVirtualScroller.outerHeight(items[i]);
+        }
+
+        //Curently scrolling the whole page, TODO option to scroll within a container
+        window.scrollBy(0, totalHeightToScroll);
+
+        return true;
+    },
+
+    outerHeight: (el) => {
+        var styles = window.getComputedStyle(el);
+        var margin = parseFloat(styles['marginTop']) +
+            parseFloat(styles['marginBottom']);
+
+        return Math.ceil(el.offsetHeight + margin);
+    }
+}
