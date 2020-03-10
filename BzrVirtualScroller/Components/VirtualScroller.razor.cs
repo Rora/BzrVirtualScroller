9a14f443c478b8e57e2a5d969e65c37cefc95a9c
Rob <2895595+Rora@users.noreply.github.com>
2020-03-10 14:31:27 +0100

    added support for a generic type param

---
+++ BzrVirtualScroller/Components/VirtualScroller.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BzrVirtualScroller.Components
+{
+    public partial class VirtualScroller<TItem>
+        where TItem : class
+    {
+    }
+}
---
+++ BzrVirtualScroller/DummyDataLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BzrVirtualScroller
+{
+    public class DummyDataLoader
+    {
+        public static async ValueTask<IEnumerable<VirtualScrollerEntry>> LoadDataAsync(ItemProducerArgs args)
+        {
+            await Task.Delay(100); //Simulate a delay to see more realistic perf
+
+            if (args.ItemId == null)
+            {
+                return (IEnumerable<VirtualScrollerEntry>)Enumerable.Range(1, args.NrOfItemsToLoad)
+                    .Select(i => new VirtualScrollerEntry { Id = i.ToString(), Value = i.ToString() })
+                    .ToArray();
+            }
+
+            var itemId = Int32.Parse(args.ItemId);
+            var items = args.ScrollDirection == ScrollDirection.Down
+                ? Enumerable.Range(itemId + 1, args.NrOfItemsToLoad)
+                : Enumerable.Range(itemId - args.NrOfItemsToLoad, args.NrOfItemsToLoad).Reverse();
+
+            return (IEnumerable<VirtualScrollerEntry>)items
+                .Select(i => new VirtualScrollerEntry { Id = i.ToString(), Value = i.ToString() })
+                .ToArray();
+        }
+    }
+}
